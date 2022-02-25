2d1a5a5891a4641d76378e56a9d9f4a490c4b80d
Sara Cormican <S_Comican@yahoo.com>
2022-02-25 15:55:26 -0500

    Firts commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { createContext, useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+import Home from './screens/Home'
+import Details from './screens/Details'
+
+export const RestaurantContext = createContext()
 
 function App() {
+	const [restaurants, setRestaurants] = useState()
+
+ useEffect (() => {
+		fetch('https://bocacode-intranet-api.web.app/restaurants')
+			.then((res) => res.json())
+			// .then((data) => console.log(data))
+			.then((data) => {
+				// console.log(data)
+				setRestaurants(data)
+			})
+			.catch(console.error)
+	}, [])
+
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+		<>
+    <RestaurantContext.Provider value={{restaurants, setRestaurants}}>
+			<Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/details/:restId' element={<Details />}/>
+      </Routes>
+    </RestaurantContext.Provider>
+		</>
+	)
 }
 
-export default App;
+export default App
---
+++ src/screens/Details.js
@@ -0,0 +1,9 @@
+export default function Details() {
+    return(
+        <>
+       <h1>hello</h1>
+</>
+
+
+    )
+}
